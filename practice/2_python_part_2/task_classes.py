568a9971b6546525d8d988a38dd4d372095a636f
tsalmon2 <126485228+tsalmon2@users.noreply.github.com>
2023-04-02 05:19:34 -0500

    Finished Part2 (#2)
    * Completed task_classes
    
    * Completed task_exceptions
    
    * Completed task_input_output
    
    * Completed task_read_write
    
    * Completed task_read_write_2
    
    * Updated task_classes.py
    
    * Updated task_exceptions to include custom exception and try finally
    
    * Updated task_read_write
    
    * Specified encoding parameter in task_read_write
    
    * Updated task_read_write to take result file path
    
    * Put functionality into a function
    
    * Updated task_read_write_2 to take in file name parameters
    
    * Added documentation for task_classes
    
    * Added documentation for task_exceptions and input_output and updated input_output
    
    * Added documentation for read write tasks
    
    * Updated read write tasks to use pathlib

--- practice/2_python_part_2/task_classes.py
+++ practice/2_python_part_2/task_classes.py
@@ -25,36 +25,64 @@ Methods:
     Note that this method doesn't need object itself
 PEP8 comply strictly.
 """
-import datetime
+from __future__ import annotations
+import datetime as dt
+from typing import Union
 
 
 class Teacher:
-    ...
+    """A class representing a Teacher object."""
 
+    def __init__(self, lname: str, fname: str) -> None:
+        self.last_name = lname
+        self.first_name = fname
 
+    def create_homework(self, homework_text: str, num_completion_days: int) -> Homework:
+        """Creates a Homework object from homework text and number of days."""
+        return Homework(homework_text, num_completion_days)
+    
+    
 class Student:
-    ...
+    """A class representing a Student object."""
+    def __init__(self, lname: str, fname: str) -> None:
+        self.last_name = lname
+        self.first_name = fname
 
+    def do_homework(self, homework: Homework) -> Union[None, Homework]:
+        """Returns Homework object if active or None if inactive."""
+        if not homework.is_active():
+            print("You are late") 
+            return None
+        return homework
+    
 
 class Homework:
-    ...
+    """A class representing a Homework object."""
+    def __init__(self, text: str, num_days_to_complete: int) -> None:
+        self.text = text
+        self.created = dt.datetime.now()
+        self.deadline = dt.timedelta(num_days_to_complete)
+
+    def is_active(self) -> bool:
+        """Returns a boolean indicating whether the Homework is active."""
+        return dt.datetime.now() <= self.created + self.deadline
 
 
 if __name__ == '__main__':
     teacher = Teacher('Dmitry', 'Orlyakov')
     student = Student('Vladislav', 'Popov')
-    teacher.last_name  # Daniil
-    student.first_name  # Petrov
+    print(teacher.last_name)  # Daniil
+    print(student.first_name)  # Petrov
 
     expired_homework = teacher.create_homework('Learn functions', 0)
-    expired_homework.created  # Example: 2019-05-26 16:44:30.688762
-    expired_homework.deadline  # 0:00:00
-    expired_homework.text  # 'Learn functions'
+    print(expired_homework.created)  # Example: 2019-05-26 16:44:30.688762
+    print(expired_homework.deadline)  # 0:00:00
+    print(expired_homework.text) # 'Learn functions'
 
     # create function from method and use it
     create_homework_too = teacher.create_homework
     oop_homework = create_homework_too('create 2 simple classes', 5)
-    oop_homework.deadline  # 5 days, 0:00:00
+    print(oop_homework.deadline)  # 5 days, 0:00:00
 
-    student.do_homework(oop_homework)
-    student.do_homework(expired_homework)  # You are late
+    print(student.do_homework(oop_homework))
+    print(student.do_homework(expired_homework))  # You are late

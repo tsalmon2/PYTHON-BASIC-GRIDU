fb955de533be05c6a355fcb1e9e516d11e33ddbf
tsalmon2 <126485228+tsalmon2@users.noreply.github.com>
2023-04-12 11:10:05 -0500

    Updated task1 to use cache

--- practice/7_concurrency/task1_fibonacci/template.py
+++ practice/7_concurrency/task1_fibonacci/template.py
@@ -1,9 +1,7 @@
 import os
 from random import randint
-import sys
 from concurrent.futures import ProcessPoolExecutor
 from pathlib import Path
-import time
 from multiprocessing import Manager
 
 
@@ -11,22 +9,14 @@ Path(Path(__file__).parent / "output").mkdir(exist_ok=True)
 OUTPUT_DIR = Path(Path(__file__).parent / "output")
 RESULT_FILE = Path(OUTPUT_DIR / 'result.csv')
 
-# Overwrite the maximum limit for integer string conversion 
-sys.set_int_max_str_digits(9999999)
-
-# def fib(n: int) -> int:
-#     """Calculate a value in the Fibonacci sequence by ordinal number"""
-#     f0, f1 = 0, 1
-#     if n==0: return 0
-#     for _ in range(n-1):
-#         f0, f1 = f1, f0 + f1
-#     return f1
-
-def fib(n: int, memo: dict) -> int:
-    if n in memo:
-        return memo[n]
-    memo[n] = fib(n-1,memo) + fib(n-2,memo)
-    return memo[n]
+def fib(n: int, memo) -> int:
+    """Calculate a value in the Fibonacci sequence by ordinal number"""
+    f0, f1 = 0, 1
+    if n in memo: return memo[n]
+    for curr in range(2,n-1):
+        f0, f1 = f1, f0 + f1
+        memo[curr] = f1
+    return f1
 
 def write_to_file(el: int, memo: dict) -> None:
     """Write the fibonnaci number to a file."""
@@ -53,10 +43,10 @@ if __name__ == '__main__':
     memo = Manager().dict()
     memo[0] = 0
     memo[1] = 1
-    start = time.time()
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
-    # func1(array=[randint(1000, 100000) for _ in range(1000)], memo=memo)
-    func1(array=[1,3,5,7,8], memo=memo)
+    nums = [randint(1000, 100000) for _ in range(1000)]
+    max_ = max(nums)
+    fib(max_, memo=memo)
+    func1(array=nums, memo=memo)
     func2(result_folder=OUTPUT_DIR)
-    print(f"time: {time.time() - start}")
